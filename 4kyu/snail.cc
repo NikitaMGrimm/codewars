450ee3031b0783ff9acb35140413864a6d6156b5
Nikita Maximilian Grimm <65914808+NikitaMGrimm@users.noreply.github.com>
2023-03-01 22:00:59 +0100

    other nice alternative solution

--- 4kyu/snail.cc
+++ 4kyu/snail.cc
@@ -62,3 +62,27 @@ std::vector<int> snail(const std::vector<std::vector<int>>& snail_map) {
   }
   return sorted;
 }
+
+
+// solution 2 from other user. I like this one better. It's short and sweet.
+
+#include <cstddef>
+#include <vector>
+
+std::vector<int> snail2(const std::vector<std::vector<int>>& xs) {
+  std::vector<int> res;
+  if (xs[0].empty())
+    return res;
+  const std::size_t ny = xs.size(), nx = xs[0].size();
+  res.reserve(ny * nx);
+  std::ptrdiff_t x0 = 0, y0 = 0, x1 = nx - 1, y1 = ny - 1, x = 0, y = 0;
+  while (y0 <= y1) {
+    while (x < x1) res.push_back(xs[y][x++]); y0++;
+    while (y < y1) res.push_back(xs[y++][x]); x1--;
+    while (x > x0) res.push_back(xs[y][x--]); y1--;
+    while (y > y0) res.push_back(xs[y--][x]); x0++;
+  }
+  res.push_back(xs[y][x]);
+  return res;
+}
+// this works by going along the rows/cols and making the accessible matrix smaller and smaller each time.
